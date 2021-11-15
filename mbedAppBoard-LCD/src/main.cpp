a21496fc1b03b63deeed4fc47c111da84aba0216
ase1590 <ase1590@hotmail.com>
2021-11-15 09:16:29 -0600

    cleanup

--- mbedAppBoard-LCD/src/main.cpp
+++ mbedAppBoard-LCD/src/main.cpp
@@ -1,3 +1,4 @@
+// sample program to display a elf and a Christmas tree animation. 
 #include "mbed.h"
 #include "C12832_lcd.h"
 #include "Arial_9.h"
@@ -14,7 +15,7 @@ int main()
     s = 3;
     lcd.print_bm(bitmTree,95,0);  // print chistmas tree
     lcd.copy_to_lcd(); 
-    lcd.setmode(XOR);             // XOR - a second print will erase  
+    lcd.setmode(XOR);        // XOR - a second print will erase  
     for(i = -15; i < 75; ){     
         lcd.print_bm(bitmSan1,i,2);
         thread_sleep_for(500);
@@ -36,22 +37,22 @@ int main()
    lcd.set_auto_up(0);
    for(i=-20; i<5; i++){             // scrolling text
      lcd.locate(5,i);
-     lcd.printf("NERDS");
+     lcd.printf("HAPPY");
      lcd.locate(5,i+12);
-     lcd.printf("AF");
+     lcd.printf("XMAS");
      lcd.copy_to_lcd();
      lcd.locate(5,i);
      thread_sleep_for(200);
-     lcd.printf("NERDS");
+     lcd.printf("HAPPY");
      lcd.locate(5,i+12);
-     lcd.printf("AF");
+     lcd.printf("XMAS");
      lcd.copy_to_lcd();
      i=i+1;
    }  
    lcd.locate(5,i);
-   lcd.printf("NERDS");
+   lcd.printf("HAPPY");
    lcd.locate(5,i+12);
-   lcd.printf("AF");
+   lcd.printf("XMAS");
    lcd.copy_to_lcd();
    
 }
